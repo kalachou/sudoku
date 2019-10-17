fd1b1332c83363b73e82562c0772d1b19d3c240f
kalachou <mvkalach>
2019-10-17 08:38:50 +0300

    sudoku(feat): backtracking algorithm is implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,103 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    //backtracking algorithm from 
+    //https://www.geeksforgeeks.org/backtracking-set-7-suduku/
+
+    function findUnassignedLocation(matrix) {
+
+        for (let i = 0; i < 9; i++) {
+
+            for (let j = 0; j < 9; j++) {
+
+                if (matrix[i][j] === 0) {
+
+                    row = i;
+                    col = j;
+                    return;
+                }
+            }
+        }
+        row = -1;
+    }
+
+    function isPossibleRow(matrix, row, num) {
+
+        for (let j = 0; j < 9; j++) {
+
+            if (matrix[row][j] === num) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function isPossibleCol(matrix, col, num) {
+
+        for (let i = 0; i < 9; i++) {
+
+            if (matrix[i][col] === num) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function isPossibleSect(matrix, row, col, num) {
+
+        for (let i = Math.floor(row / 3) * 3, lastRowIndex = i + 3; i < lastRowIndex; i++) {
+
+            for (let j = Math.floor(col / 3) * 3, lastColIndex = j + 3; j < lastColIndex; j++) {
+
+                if (matrix[i][j] === num) return false;
+            }
+        }
+
+        return true;
+    }
+
+    function isPossible(matrix, row, col, num) {
+
+        const isPossible = isPossibleRow(matrix, row, num)
+            && isPossibleCol(matrix, col, num)
+            && isPossibleSect(matrix, row, col, num);
+
+        return isPossible;
+    }
+
+    let row;
+    let col;
+
+    findUnassignedLocation(matrix);
+
+    if (row === -1) {
+        //solved
+        return matrix;
+    }
+
+    for (let suggest = 1; suggest < 10; suggest++) {
+
+        //if suggest is possible
+        if (isPossible(matrix, row, col, suggest)) {
+
+            //make suggest 
+            matrix[row][col] = suggest;
+
+            //if suggest will lead to solution
+            if (solveSudoku(matrix)) {
+                return matrix;
+            }
+
+            //else trying another suggest
+            else {
+                matrix[row][col] = 0;
+            }
+        }
+
+        if (suggest === 9) {
+            return;
+        }
+    }
+
+    return matrix;
 }
